832d42aaa0ae1d63e837704ddfc19b3b25349f87
shamspias <shamsuddinpias0@gmail.com>
2023-03-31 12:40:10 +0600

    fix: forcast and identification

--- src/web_app/app.py
+++ src/web_app/app.py
@@ -1,66 +1,63 @@
 # app.py
-from flask import Flask, request
-from flask_restful import Resource, Api
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-import joblib
-from PIL import Image
 
-# Import the required functions from your project
-# from src.disease_forecasting.forecasting import load_forecasting_model, forecast
-# from src.disease_identification.identification import load_identification_model, identify
+from flask import Flask, request, jsonify
+from src.utils import utilities
+from src.disease_identification import identification
+from src.disease_forecasting import forecasting
+from src.data_preprocessing import data_preprocessing
 
 app = Flask(__name__)
-api = Api(app)
 
 
-# Load the disease forecasting and identification models
-# forecasting_model = load_forecasting_model("models/disease_forecasting/forecasting_model.pkl")
-# identification_model = load_identification_model("models/disease_identification/identification_model.h5")
-
-class DiseaseForecast(Resource):
+@app.route('/api/forecast', methods=['POST'])
+def forecast_disease():
     """
-    This class is used to forecast the disease based on the input data.
+    Forecast the disease based on the climate data
+    :return: JSON object containing the forecasted disease
     """
+    climate_data = request.json
 
-    def post(self):
-        """
-        This method is used to forecast the disease based on the input data.
-        :return:    The prediction of the disease
-        """
-        data = request.get_json()
+    # Preprocess input data
+    preprocessed_data = data_preprocessing.preprocess_input_data(climate_data)
 
-        # Replace the placeholder code with the actual code to use the forecasting model
-        # prediction = forecast(forecasting_model, data)
-        prediction = "forecast_result"
+    # Load the trained model for forecasting
+    model_file = 'models/disease_forecasting/forecasting_model.h5'
+    model = utilities.load_model(model_file)
 
-        return {"prediction": prediction}
+    # Forecast the disease
+    forecast_result = forecasting.forecast_disease(model, preprocessed_data)
+
+    # Format and return the result as a JSON object
+    response = {
+        'disease_forecast': forecast_result
+    }
+    return jsonify(response)
 
 
-class DiseaseIdentification(Resource):
+@app.route('/api/identify', methods=['POST'])
+def identify_disease():
     """
-    This class is used to identify the disease based on the input image.
+    Identify the disease based on the image
+    :return: JSON object containing the identified disease
     """
+    image_file = request.files['image']
 
-    def post(self):
-        """
-        This method is used to identify the disease based on the input image.
-        :return:    The prediction of the disease
-        """
-        image = request.files['image']
-        img = Image.open(image)
+    # Load the image data
+    image_data = utilities.load_image_data(image_file)
 
-        # Replace the placeholder code with the actual code to use the identification model
-        # prediction = identify(identification_model, img)
-        prediction = "identification_result"
+    # Load the trained model for identification
+    model_file = 'models/disease_identification/identification_model.h5'
+    model = utilities.load_model(model_file)
 
-        return {"prediction": prediction}
+    # Identify the disease based on the image
+    disease_identification_result = identification.identify_disease(model, image_data)
 
+    # Format and return the result as a JSON object
+    response = {
+        'disease_identification': disease_identification_result
+    }
+    return jsonify(response)
 
-#   Add the API resources
-api.add_resource(DiseaseForecast, '/api/forecast')
-api.add_resource(DiseaseIdentification, '/api/identify')
 
 if __name__ == '__main__':
     app.run(debug=True)
