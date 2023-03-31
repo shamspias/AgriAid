c09277f98fbab8d5c4b53aa8b42d6ac2ba48a09a
shamspias <shamsuddinpias0@gmail.com>
2023-03-31 15:28:14 +0600

    fix: utilities

--- src/web_app/utils/utilities.py
+++ src/web_app/utils/utilities.py
@@ -1,21 +1,21 @@
 import os
+import random
 import numpy as np
 from PIL import Image
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-import random
+from sklearn.preprocessing import LabelEncoder
 import tensorflow as tf
-import keras
+from tensorflow import keras
 
 RANDOM_SEED = 42
 
 
 def load_images_from_directory(directory, target_size=(224, 224)):
     """
-    Load images from the specified directory.
-    :param directory: Directory containing the images
-    :param target_size: Target size of the images
-    :return: Images and their corresponding labels
+    Load images and their labels from a directory.
+    :param directory: Path to the directory containing images
+    :param target_size: Tuple specifying the desired dimensions of the resized images
+    :return: List of images and their corresponding labels
     """
     images = []
     labels = []
@@ -37,11 +37,11 @@ def load_images_from_directory(directory, target_size=(224, 224)):
 
 def prepare_data(images, labels, test_size=0.2):
     """
-    Prepare data for training and testing.
+    Split the image and label data into training and testing sets.
     :param images: List of images
-    :param labels: List of labels
-    :param test_size: The proportion of the dataset to include in the test split
-    :return: Training and testing data along with the label encoder
+    :param labels: List of corresponding labels
+    :param test_size: Proportion of the dataset to include in the test split (default: 0.2)
+    :return: Train and test sets for image data and labels, and the label encoder
     """
     X = np.array(images)
     y = np.array(labels)
@@ -54,20 +54,34 @@ def prepare_data(images, labels, test_size=0.2):
     return X_train, X_test, y_train, y_test, le
 
 
+def load_image_data(image_file, target_size=(224, 224)):
+    """
+    Load image data from a file.
+    :param image_file: Image file
+    :param target_size: Tuple specifying the desired dimensions of the resized image
+    :return: Image array
+    """
+    img = Image.open(image_file).resize(target_size)
+    img_array = np.array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
+
 def save_model(model, output_file):
     """
-    Save the trained model to the specified file.
-    :param model: The trained model
-    :param output_file: The file to save the model to
+    Save a trained model to a file.
+    :param model: Trained model
+    :param output_file: Path to the output file
+    :return: None
     """
     model.save(output_file)
 
 
 def load_model(model_file):
     """
-    Load the trained model from the specified file.
-    :param model_file: The file containing the trained model
-    :return: The trained model
+    Load a trained model from a file.
+    :param model_file: Path to the model file
+    :return: Loaded model
     """
     return keras.models.load_model(model_file)
 
@@ -75,7 +89,8 @@ def load_model(model_file):
 def set_random_seed(seed_value):
     """
     Set the random seed for reproducibility.
-    :param seed_value: The seed value
+    :param seed_value: Seed value
+    :return: None
     """
     np.random.seed(seed_value)
     random.seed(seed_value)
