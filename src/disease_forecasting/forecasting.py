dd336fb37de3a46e5c785d654df8d38f519645e7
shamspias <shamsuddinpias0@gmail.com>
2023-03-31 13:06:52 +0600

    fix: forcast model

--- src/disease_forecasting/forecasting.py
+++ src/disease_forecasting/forecasting.py
@@ -14,27 +14,44 @@ def load_preprocessed_data(file_path):
 
 
 def prepare_data(df, target_col, test_size=0.2):
+    """
+    Prepare data for training
+    :param df:  data
+    :param target_col:  target column
+    :param test_size:   test size
+    :return:    training and test data, label encoder
+    """
     X = df.drop(columns=[target_col])
     y = df[target_col]
 
     le = LabelEncoder()
     y = le.fit_transform(y)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=RANDOM_SEED)
-
-    return X_train, X_test, y_train, y_test, le
+    return train_test_split(X, y, test_size=test_size, random_state=RANDOM_SEED), le
 
 
 def train_forecasting_model(X_train, y_train):
+    """
+    Train forecasting model
+    :param X_train:  training data
+    :param y_train:  training labels
+    :return:       trained forecasting model
+    """
     model = xgb.XGBClassifier(use_label_encoder=False, eval_metric='mlogloss', random_state=RANDOM_SEED)
     model.fit(X_train, y_train)
     return model
 
 
 def evaluate_model(model, X_test, y_test):
+    """
+    Evaluate forecasting model
+    :param model:   forecasting model
+    :param X_test:  test data
+    :param y_test:  test labels
+    :return:    accuracy score
+    """
     y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    return accuracy
+    return accuracy_score(y_test, y_pred)
 
 
 def save_model(model, output_file):
@@ -45,20 +62,15 @@ def main():
     input_file = 'data/preprocessed/preprocessed_data.csv'
     output_file = 'models/disease_forecasting/forecasting_model.pkl'
 
-    # Load preprocessed data
     data = load_preprocessed_data(input_file)
 
-    # Prepare data for training and testing
-    X_train, X_test, y_train, y_test, label_encoder = prepare_data(data, target_col='disease')
+    (X_train, X_test, y_train, y_test), label_encoder = prepare_data(data, target_col='disease')
 
-    # Train the forecasting model
     model = train_forecasting_model(X_train, y_train)
 
-    # Evaluate the model
     accuracy = evaluate_model(model, X_test, y_test)
     print(f"Model accuracy: {accuracy:.4f}")
 
-    # Save the trained model
     save_model(model, output_file)
 
 
