d63bb6ab1609d29a43d4a4c3eb09c38a6b3b06d0
shamspias <shamsuddinpias0@gmail.com>
2023-03-31 15:22:18 +0600

    fix: forecast disease

--- src/disease_forecasting/forecasting.py
+++ src/disease_forecasting/forecasting.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 import pandas as pd
 import xgboost as xgb
@@ -10,16 +9,21 @@ RANDOM_SEED = 42
 
 
 def load_preprocessed_data(file_path):
+    """
+    Load preprocessed data from the given file path.
+    :param file_path: Path to the preprocessed data file
+    :return: A pandas DataFrame containing the preprocessed data
+    """
     return pd.read_csv(file_path)
 
 
 def prepare_data(df, target_col, test_size=0.2):
     """
-    Prepare data for training
-    :param df:  data
-    :param target_col:  target column
-    :param test_size:   test size
-    :return:    training and test data, label encoder
+    Prepare data for training and testing.
+    :param df: DataFrame containing the preprocessed data
+    :param target_col: Column name containing the target variable (disease)
+    :param test_size: Proportion of the dataset to include in the test split
+    :return: X_train, X_test, y_train, y_test, and a label encoder object
     """
     X = df.drop(columns=[target_col])
     y = df[target_col]
@@ -27,15 +31,17 @@ def prepare_data(df, target_col, test_size=0.2):
     le = LabelEncoder()
     y = le.fit_transform(y)
 
-    return train_test_split(X, y, test_size=test_size, random_state=RANDOM_SEED), le
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=RANDOM_SEED)
+
+    return X_train, X_test, y_train, y_test, le
 
 
 def train_forecasting_model(X_train, y_train):
     """
-    Train forecasting model
-    :param X_train:  training data
-    :param y_train:  training labels
-    :return:       trained forecasting model
+    Train the disease forecasting model using X_train and y_train.
+    :param X_train: Training data
+    :param y_train: Training labels
+    :return: A trained forecasting model
     """
     model = xgb.XGBClassifier(use_label_encoder=False, eval_metric='mlogloss', random_state=RANDOM_SEED)
     model.fit(X_train, y_train)
@@ -44,35 +50,35 @@ def train_forecasting_model(X_train, y_train):
 
 def evaluate_model(model, X_test, y_test):
     """
-    Evaluate forecasting model
-    :param model:   forecasting model
-    :param X_test:  test data
-    :param y_test:  test labels
-    :return:    accuracy score
+    Evaluate the forecasting model on the test data.
+    :param model: The trained forecasting model
+    :param X_test: Test data
+    :param y_test: Test labels
+    :return: Model accuracy
     """
     y_pred = model.predict(X_test)
-    return accuracy_score(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
 
 
 def save_model(model, output_file):
+    """
+    Save the trained model to the specified output file.
+    :param model: The trained forecasting model
+    :param output_file: Path to the output file
+    """
     model.save_model(output_file)
 
 
-def main():
-    input_file = 'data/preprocessed/preprocessed_data.csv'
-    output_file = 'models/disease_forecasting/forecasting_model.pkl'
-
-    data = load_preprocessed_data(input_file)
-
-    (X_train, X_test, y_train, y_test), label_encoder = prepare_data(data, target_col='disease')
-
-    model = train_forecasting_model(X_train, y_train)
-
-    accuracy = evaluate_model(model, X_test, y_test)
-    print(f"Model accuracy: {accuracy:.4f}")
-
-    save_model(model, output_file)
-
-
-if __name__ == '__main__':
-    main()
+def forecast_disease(model, preprocessed_data):
+    """
+    Forecast the disease using the trained model and preprocessed input data.
+    :param model: The trained forecasting model
+    :param preprocessed_data: Preprocessed input data
+    :return: Disease label
+    """
+    input_data = xgb.DMatrix(preprocessed_data)
+    predictions = model.predict(input_data)
+    disease_index = np.argmax(predictions)
+    disease_label = model.classes_[disease_index]
+    return disease_label
